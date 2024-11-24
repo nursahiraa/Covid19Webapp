f1c8b037089db4e9e327708c5480e7206a336bb1
Nur Sahira Zhamri <znursahira02@gmail.com>
2024-11-24 11:14:25 +0000

    update apps.py: always fetch and update data when server starts every single time

--- api/apps.py
+++ api/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
+
+    def ready(self):
+        from api.utils import fetch_and_update_data
+        try:
+            fetch_and_update_data()
+        except Exception as e:
+            print(f"Error updating data on server start: {e}")

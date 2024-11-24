f78f1112819b702d0a3983e6a08b4b517000c39f
Nur Sahira Zhamri <znursahira02@gmail.com>
2024-11-24 11:19:30 +0000

    remove unnecessary files

--- api/management/commands/fetch_and_update_data.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-from api.utils import fetch_and_update_data
-
-class Command(BaseCommand):
-    help = 'Fetch and update COVID-19 data'
-
-    def handle(self, *args, **kwargs):
-        fetch_and_update_data()
-        self.stdout.write("Data updated successfully.")

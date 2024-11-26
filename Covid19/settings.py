ede8661640a63c1b77f87eebf5bb08d282727d2f
Nur Sahira Zhamri <znursahira02@gmail.com>
2024-11-26 09:29:09 +0000

    update database: change from sqlite3 database to mySQL database

--- Covid19/settings.py
+++ Covid19/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'Covid19.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Covid19',  # Replace with your database name
+        'USER': 'root',              # Replace with your MySQL user
+        'PASSWORD': '',      # Replace with your MySQL password
+        'HOST': 'localhost',              # Keep localhost for local setup
+        'PORT': '3306',                   # Default MySQL port
     }
 }
+
 
 
 # Password validation
